934f67e18fef8ef88b6742add7c1408ab5321f4c
Patrick Chung <kchungsemchong@gmail.com>
2016-01-20 10:21:13 +0400

    Add Supplier Class

---
+++ ECommerce.WebApi/Models/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerce.WebApi.Models
+{
+    [Table("SupplierSummary")]
+    public class SupplierSummaryViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ProductSummaryViewModel> Products { get; set; }
+    }
+
+    [Table("SupplierDetails")]
+    public class SupplierViewModel : SupplierSummaryViewModel
+    {
+        public string Website { get; set; }
+    }
+}
