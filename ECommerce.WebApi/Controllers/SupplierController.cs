09b836b6cfeb77ff10742d0bd274acd7157d1099
Patrick Chung <kchungsemchong@gmail.com>
2016-01-20 10:28:22 +0400

    Add Supplier Controller

---
+++ ECommerce.WebApi/Controllers/SupplierController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.OData;
+using ECommerce.WebApi.Models;
+
+namespace ECommerce.WebApi.Controllers
+{
+    [RoutePrefix("api/supplier")]
+    public class SupplierController : ApiController
+    {
+        private readonly ISupplierRepo _repository;
+
+        public SupplierController(ISupplierRepo repo)
+        {
+            _repository = repo;
+        }
+
+        [EnableQuery()]
+        [ResponseType(typeof(SupplierSummaryViewModel))]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(_repository.Retrieve().AsQueryable());
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
