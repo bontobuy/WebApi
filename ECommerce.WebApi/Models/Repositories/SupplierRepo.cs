d1f6f665065912e0cffa32935dea889d00567eeb
Patrick Chung <kchungsemchong@gmail.com>
2016-01-20 10:31:43 +0400

    Add supplier repo

---
+++ ECommerce.WebApi/Models/Repositories/SupplierRepo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ECommerce.WebApi.Models
+{
+    public class SupplierRepo : ISupplierRepo
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public IEnumerable<SupplierSummaryViewModel> Retrieve()
+        {
+            var records = db.SupplierSummary.ToList();
+
+            return records;
+        }
+    }
+}
