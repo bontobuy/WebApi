d0cd4ac25367b9f84d8e65e2129c4fedced7d375
Patrick Chung <kchungsemchong@gmail.com>
2016-01-20 07:23:02 +0400

    Add Product Class

---
+++ ECommerce.WebApi/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.WebApi.Models.ViewModels
+{
+    [Table("ProductSummary")]
+    public class ProductSummaryViewModel
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("SupplierId")]
+        public SupplierSummaryViewModel Supplier { get; set; }
+    }
+
+    [Table("ProductDetails")]
+    public class ProductViewModel : ProductSummaryViewModel
+    {
+        public string Highlights { get; set; }
+    }
+}
