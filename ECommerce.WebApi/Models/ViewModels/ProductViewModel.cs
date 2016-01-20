7a056cc7ad0f43b5c9e931dd08fc8cff64094f76
Patrick Chung <kchungsemchong@gmail.com>
2016-01-20 10:22:22 +0400

    Remove unused assemblies

--- ECommerce.WebApi/Models/ViewModels/ProductViewModel.cs
+++ ECommerce.WebApi/Models/ViewModels/ProductViewModel.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
-namespace ECommerce.WebApi.Models.ViewModels
+namespace ECommerce.WebApi.Models
 {
     [Table("ProductSummary")]
     public class ProductSummaryViewModel
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductId { get; set; }
         public int SupplierId { get; set; }
         public string Name { get; set; }
