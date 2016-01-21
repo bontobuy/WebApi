29789d289632d7f265615527e3b23dc6e726dd71
Patrick Chung <kchungsemchong@gmail.com>
2016-01-21 14:20:07 +0400

    Add Interface Product

---
+++ ECommerce.WebApi/Models/Interfaces/IProductRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.WebApi.Models
+{
+    public interface IProductRepo
+    {
+        IEnumerable<ProductSummaryViewModel> Retrieve();
+        ProductViewModel Get(int id);
+        ProductViewModel Create(ProductViewModel item);
+        ProductViewModel Update(int id, ProductViewModel item);
+        void Remove(int id);
+    }
+}
