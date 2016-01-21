84671f6843d85b1ceda4886c1797af263be07914
Patrick Chung <kchungsemchong@gmail.com>
2016-01-21 22:07:05 +0400

    Add Brand View Model

---
+++ ECommerce.WebApi/Models/ViewModels/BrandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.WebApi.Models
+{
+    [Table("BrandDetails")]
+    public class BrandViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+
+        //public IEnumerable<ProductSummaryViewModel> BrandProduct { get; set; }
+    }
+}
